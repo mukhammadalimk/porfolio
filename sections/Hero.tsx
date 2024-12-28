449fabb3fbf8bd23e9282c8f83518db020dd5d67
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-12-28 23:09:44 +0900

    added Projects section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,9 +1,9 @@
-import memojiImage from "../assets/images/memoji-computer.png";
-import ArrowDown from "../assets/icons/arrow-down.svg";
+import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 import Image from "next/image";
-import grainImage from "../assets/images/grain.jpg";
-import StarIcon from "../assets/icons/star.svg";
-import SparkleIcon from "../assets/icons/sparkle.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 import HeroOrbit from "@/components/HeroOrbit";
 
 const HeroSection = () => {
