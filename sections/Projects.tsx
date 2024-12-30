97f406a5f251b829c72fcf7083c8c401f96badb6
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-12-30 20:21:09 +0900

    made projects be able to be stacked on top of each other by making them sticky

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -59,10 +59,13 @@ export const ProjectsSection = () => {
 
         {/* Projects list */}
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
-              className="px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
               key={project.title}
+              className="px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
